4bf6265354f7fd61be2f3f67c0df3adb409a53af
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-01 14:14:01 -0500

    solve twoSums problem with one-pass hash table in Java

--- arrays/0001-two-sums/Solution.java
+++ arrays/0001-two-sums/Solution.java
@@ -1,6 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
   // Brute force
   // Time Complexity: O(n^2)
+  // Space Complexity: O(1)
   public int[] twoSum1(int[] nums, int target) {
     int length = nums.length;
     for (int i = 0; i < length - 1; i++) {
@@ -15,6 +19,7 @@ class Solution {
 
   // Consider the target's constraints -10^9 <= target <= 10^9
   // Time Complexity: O(n^2)
+  // Space Complexity: O(1)
   public int[] twoSum2(int[] nums, int target) {
     int length = nums.length;
     for (int i = 0; i < length - 1; i++) {
@@ -26,4 +31,23 @@ class Solution {
     }
     return null;
   }
+
+  // One-pass Hash Table
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+  public int[] twoSum3(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+      if (map.containsKey(complement)) {
+        return new int[] { i, map.get(complement) };
+      }
+      map.put(nums[i], i);
+    }
+    return null;
+  }
+  // Lesson:
+  // Use hash table to re-organize the given data structure for more efficient
+  // query. Put the easily accessible property as key, put property to query as
+  // the value.
 }
