80699aaa481e8c27c207c0e96b77ad435df8fb82
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-02 00:55:35 -0500

    solve problem 21 with recursion in Java

--- linked-list/0021-merge-two-sorted-lists/Solution.java
+++ linked-list/0021-merge-two-sorted-lists/Solution.java
@@ -2,7 +2,7 @@ public class Solution {
   // Two Pointers
   // Time Complexity: O(n1+n2)
   // Space Complexity: O(1)
-  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+  public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
     ListNode dummy = new ListNode();
     ListNode node1 = list1;
     ListNode node2 = list2;
@@ -30,6 +30,23 @@ public class Solution {
       node2 = node2.next;
     }
     return dummy.next;
+  }
+
+  // Recursion
+  // Time Complexity: O(n1+n2)
+  // Space Complexity: O(n1+n2) number of stack frames
+  public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+    if (list1 == null)
+      return list2;
+    if (list2 == null)
+      return list1;
+    if (list1.val <= list2.val) {
+      list1.next = mergeTwoLists2(list1.next, list2);
+      return list1;
+    } else {
+      list2.next = mergeTwoLists2(list1, list2.next);
+      return list2;
+    }
   }
 }
 
