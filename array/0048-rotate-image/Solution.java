a5b32b1641782c000dab759f3825eca707b19e39
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-02 12:31:33 -0500

    solve problem 48 with "transpose + reverse = rotate" in Java

--- array/0048-rotate-image/Solution.java
+++ array/0048-rotate-image/Solution.java
@@ -2,7 +2,7 @@ public class Solution {
   // In-place Rotation (every four-cell pairs)
   // Time Complexity: O(M)
   // Space Complexity: O(1)
-  public void rotate(int[][] matrix) {
+  public void rotate1(int[][] matrix) {
     int side = matrix.length - 1;
     for (int i = 0; i <= (side - 1) / 2; i++) {
       for (int j = i; j < side - i; j++) {
@@ -16,4 +16,40 @@ public class Solution {
   }
   // Lesson:
   // Four cells in a rotation group has connections in their indices.
+
+  // Transpose + Reverse (horizontally) = Rotate (clockwise 90 degree)
+  // Time Complexity: O(M)
+  // Space Complexity: O(1)
+  public void rotate2(int[][] matrix) {
+    transpose(matrix);
+    reverse(matrix);
+  }
+  // Lesson:
+  // Three ways to transform a matrix
+  // -- mathematical way: linear algebra
+  // -- trace cells in a transformation group using patterns of their indices
+  // -- divide transformation into basic and easy-to-implement types (mirror,
+  // translation)
+
+  private static void transpose(int[][] matrix) {
+    int side = matrix.length;
+    for (int i = 0; i < side - 1; i++) {
+      for (int j = i + 1; j < side; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = tmp;
+      }
+    }
+  }
+
+  private static void reverse(int[][] matrix) {
+    int side = matrix.length;
+    for (int i = 0; i < side; i++) {
+      for (int j = 0; j < side / 2; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[i][side - j - 1];
+        matrix[i][side - j - 1] = tmp;
+      }
+    }
+  }
 }
