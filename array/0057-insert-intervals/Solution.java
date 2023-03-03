5d2af20244638b64f5512e118bcd0d389a893474
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-03 11:23:39 -0500

    solve problem 57 with linear search in Java

---
+++ array/0057-insert-intervals/Solution.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+  // Linear Search
+  // Time Complexity: O(n)
+  // Space Complexity: O(1)
+  public int[][] insert(int[][] intervals, int[] newInterval) {
+    int[][] insertedIntervals = insertNewInterval(intervals, newInterval);
+    return mergeNewInterval(insertedIntervals);
+  }
+  // Lesson:
+  // Construct a new ArrayList from an existing array doesn't require extra RAM
+  // space because it simply wraps/references the array and equips it with more
+  // functions.
+
+  private static int[][] insertNewInterval(int[][] intervals, int[] newInterval) {
+    // Data Structure (no extra space)
+    List<int[]> intervalsList = new ArrayList<>(Arrays.asList(intervals));
+    // Variable Init
+    boolean isInserted = false;
+    // Traverse
+    for (int i = 0; i < intervals.length; i++) {
+      int[] interval = intervals[i];
+      if (interval[0] > newInterval[0]) {
+        isInserted = true;
+        intervalsList.add(i, newInterval);
+        break;
+      }
+    }
+    if (!isInserted)
+      intervalsList.add(newInterval);
+    return intervalsList.toArray(new int[intervalsList.size()][2]);
+  }
+
+  private static int[][] mergeNewInterval(int[][] intervals) {
+    // Data Structure (return solution)
+    List<int[]> intervalsList = new ArrayList<>();
+    // Traverse
+    for (int i = 0; i < intervals.length; i++) {
+      int[] interval = intervals[i];
+      while (i + 1 < intervals.length &&
+          doesIntervalsOverlap(interval, intervals[i + 1])) {
+        interval[0] = Math.min(interval[0], intervals[i + 1][0]);
+        interval[1] = Math.max(interval[1], intervals[i + 1][1]);
+        i++;
+      }
+      intervalsList.add(interval);
+    }
+    return intervalsList.toArray(new int[intervalsList.size()][2]);
+  }
+
+  private static boolean doesIntervalsOverlap(int[] interval1, int[] interval2) {
+    return Math.min(interval1[1], interval2[1]) >= Math.max(interval1[0], interval2[0]);
+  }
+}
