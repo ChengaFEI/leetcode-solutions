851a66182c72f9674b2bbbe606a8474820a7c621
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-03 04:28:36 -0500

    solve problem 56 with "graph traversal" in Java

---
+++ array/0056-merge-intervals/Solution.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
+public class Solution {
+  // Graph Traversal
+  // Time Complexity: O(n^2)
+  // Space Complexity: O(n^2)
+  public int[][] merge1(int[][] intervals) {
+    // Step 0: Data Structure
+    Map<int[], List<int[]>> graph = new HashMap<>();
+    Map<Integer, List<int[]>> groups = new HashMap<>();
+    List<int[]> merged = new ArrayList<>();
+    // Step 2: Traverse
+    // Build Graph
+    buildGraph(intervals, graph);
+    // Categorize Groups
+    buildGroups(graph, groups);
+    // Merge Groups
+    mergeGroups(groups, merged);
+    return merged.toArray(new int[merged.size()][]);
+  }
+  // Lesson:
+  // Graph is useful in grouping nodes with same features but are not directly
+  // related to each other.
+
+  private static void buildGraph(int[][] intervals, Map<int[], List<int[]>> graph) {
+    for (int[] interval : intervals)
+      graph.put(interval, new ArrayList<int[]>());
+    for (int[] interval1 : intervals) {
+      for (int[] interval2 : intervals) {
+        if (isOverlapped(interval1, interval2)) {
+          graph.get(interval1).add(interval2);
+          graph.get(interval2).add(interval1);
+        }
+      }
+    }
+  }
+
+  private static boolean isOverlapped(int[] interval1, int[] interval2) {
+    return interval1[1] >= interval2[0] && interval1[0] <= interval2[1];
+  }
+
+  private static void buildGroups(Map<int[], List<int[]>> graph, Map<Integer, List<int[]>> groups) {
+    Set<int[]> visited = new HashSet<>();
+    int index = 0;
+    for (int[] interval : graph.keySet()) {
+      if (!visited.contains(interval)) {
+        Stack<int[]> toVisit = new Stack<>();
+        toVisit.push(interval);
+        while (!toVisit.isEmpty()) {
+          int[] curInterval = toVisit.pop();
+          visited.add(curInterval);
+          if (!groups.containsKey(index))
+            groups.put(index, new ArrayList<>());
+          groups.get(index).add(curInterval);
+          for (int[] childInterval : graph.get(curInterval)) {
+            if (!visited.contains(childInterval))
+              toVisit.push(childInterval);
+          }
+        }
+        index++;
+      }
+    }
+  }
+
+  private static void mergeGroups(Map<Integer, List<int[]>> groups, List<int[]> merged) {
+    for (List<int[]> group : groups.values())
+      merged.add(mergeGroup(group));
+  }
+
+  private static int[] mergeGroup(List<int[]> group) {
+    int minVal = group.get(0)[0], maxVal = group.get(0)[1];
+    for (int[] interval : group) {
+      minVal = Math.min(minVal, interval[0]);
+      maxVal = Math.max(maxVal, interval[1]);
+    }
+    return new int[] { minVal, maxVal };
+  }
+}
