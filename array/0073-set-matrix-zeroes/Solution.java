b5c22ed0ce5af4fd3d2663fe3794ae07d6197aa2
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-03 19:56:30 -0500

    solve problem 73 with "in-place replacement" in Java

--- array/0073-set-matrix-zeroes/Solution.java
+++ array/0073-set-matrix-zeroes/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
   // Two-Pass Linear Traversal
   // Time Complexity: O(xy)
   // Space Complexity: O(x+y)
-  public void setZeroes(int[][] matrix) {
+  public void setZeroes1(int[][] matrix) {
     // Corner Case
     if (matrix == null || matrix.length == 0)
       return;
@@ -27,6 +27,47 @@ public class Solution {
         if (xToZero[i] || yToZero[j])
           matrix[i][j] = 0;
       }
+    }
+  }
+
+  // In-place Replacement
+  // Time Complexity: O(mn)
+  // Space Complexity: O(1)
+  public void setZeroes2(int[][] matrix) {
+    // Corner Case
+    if (matrix == null || matrix.length == 0)
+      return;
+    // Variable Init
+    int xSide = matrix.length, ySide = matrix[0].length;
+    boolean colToZero = false;
+    // Traverse
+    for (int i = 0; i < xSide; i++) {
+      if (matrix[i][0] == 0)
+        colToZero = true;
+      for (int j = 1; j < ySide; j++) {
+        if (matrix[i][j] == 0) {
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+        }
+      }
+    }
+    for (int i = 1; i < xSide; i++) {
+      if (matrix[i][0] == 0) {
+        for (int j = 1; j < ySide; j++)
+          matrix[i][j] = 0;
+      }
+    }
+    for (int j = 1; j < ySide; j++) {
+      if (matrix[0][j] == 0) {
+        for (int i = 1; i < xSide; i++)
+          matrix[i][j] = 0;
+      }
+    }
+    if (matrix[0][0] == 0)
+      Arrays.fill(matrix[0], 0);
+    if (colToZero) {
+      for (int i = 0; i < xSide; i++)
+        matrix[i][0] = 0;
     }
   }
 }
