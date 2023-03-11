39ddac296f0d0570334128d142ad820a0d0bde5a
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-11 13:27:28 -0500

    solve problem 102 with iteration in Java

--- tree/0102-binary-tree-level-order-traversal/Solution.java
+++ tree/0102-binary-tree-level-order-traversal/Solution.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Solution {
-  // Recurse
+  // Recursion
   // Time Complexity: O(n)
   // Space Complexity: O(n)
   public List<List<Integer>> levelOrder1(TreeNode root) {
@@ -28,4 +30,37 @@ public class Solution {
     levelOrderRec(node.left, levels, level + 1);
     levelOrderRec(node.right, levels, level + 1);
   }
+
+  // Iteration
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+  public List<List<Integer>> levelOrder2(TreeNode root) {
+    // Corner Case
+    List<List<Integer>> levels = new ArrayList<>(); // return result
+    if (root == null)
+      return levels;
+    // Data Structure
+    Queue<TreeNode> nodes = new LinkedList<>();
+    // Variable Init
+    nodes.add(root);
+    int level = 0;
+    // Iterate
+    while (!nodes.isEmpty()) {
+      levels.add(new ArrayList<>());
+      int size = nodes.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = nodes.remove();
+        levels.get(level).add(node.val);
+        if (node.left != null)
+          nodes.add(node.left);
+        if (node.right != null)
+          nodes.add(node.right);
+      }
+      level++;
+    }
+    return levels;
+  }
+  // Lesson:
+  // Flags in the recursion is kept and updated in the recursive function, but
+  // flags in the iteration is kept and updated in the while loop.
 }
