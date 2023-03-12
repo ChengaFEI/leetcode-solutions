045dc78771f2b347a2ac168d8c671232d5a111ee
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-12 01:33:14 -0500

    solve problem 128 with hash table in Java

--- array/0128-longest-consecutive-sequence/Solution.java
+++ array/0128-longest-consecutive-sequence/Solution.java
@@ -1,11 +1,13 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
   // Sorting + One-Pass Iteration
   // Time Complexity: O(nlogn)
   // Space Complexity: O(1)/O(n) (depends on whether the input array is
   // modifiable)
-  public int longestConsecutive(int[] nums) {
+  public int longestConsecutive1(int[] nums) {
     // Corner Case
     if (nums == null || nums.length == 0)
       return 0;
@@ -26,4 +28,33 @@ public class Solution {
     }
     return Math.max(maxStreak, curStreak);
   }
+
+  // HashTable
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+  public int longestConsecutive2(int[] nums) {
+    // Corner Case
+    if (nums == null || nums.length == 0)
+      return 0;
+    // Data Structure
+    Set<Integer> numSet = new HashSet<>();
+    // Variable Init
+    int maxStreak = 1, curStreak = 1;
+    // Iterate
+    for (int num : nums)
+      numSet.add(num);
+    for (int num : numSet) {
+      if (!numSet.contains(num - 1)) {
+        while (numSet.contains(num + 1)) {
+          curStreak++;
+          num++;
+        }
+        maxStreak = Math.max(maxStreak, curStreak);
+        curStreak = 1;
+      }
+    }
+    return maxStreak;
+  }
+  // Lesson:
+  // One of the hash table's advantages is to ensure contant time querying.
 }
