73d06cec9fff212903c92bbeb4e649b954182dcc
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-03-11 21:33:52 -0500

    solve problem 104 with iteration in Java

--- tree/0104-maximum-depth-of-binary-tree/Solution.java
+++ tree/0104-maximum-depth-of-binary-tree/Solution.java
@@ -1,10 +1,13 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
   private static int maxDepth;
 
   // Recursion
   // Time Complexity: O(n)
   // Space Complexity: O(n)
-  public int maxDepth(TreeNode root) {
+  public int maxDepth1(TreeNode root) {
     maxDepth = 0;
     maxDepthRec(root, 0);
     return maxDepth;
@@ -23,5 +26,32 @@ public class Solution {
       maxDepthRec(node.left, curLevel + 1);
       maxDepthRec(node.right, curLevel + 1);
     }
+  }
+
+  // Iteration
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+  public int maxDepth2(TreeNode root) {
+    // Corner Case
+    if (root == null)
+      return 0;
+    // Data Structure
+    Queue<TreeNode> nodes = new LinkedList<>();
+    // Variable Init
+    int maxDepth = 0;
+    nodes.add(root);
+    // Iterate
+    while (!nodes.isEmpty()) {
+      int size = nodes.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = nodes.remove();
+        if (node.left != null)
+          nodes.add(node.left);
+        if (node.right != null)
+          nodes.add(node.right);
+      }
+      maxDepth++;
+    }
+    return maxDepth;
   }
 }
